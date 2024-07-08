b8e44dc123adefc078d6f72bc3dd0b94e653e5d1
ravindra <ravindra.cloudgen.in>
2024-07-08 18:13:53 +0530

    commit

--- comparsion.py
+++ comparsion.py
@@ -5,3 +5,4 @@ if(n1>n2):
     print("n1 is big ",n1)
 else:
     ("n2 is  big ",n2)
+   
---
+++ comparsion1.py
@@ -0,0 +1,28 @@
+n1=int(input("enter a value n1 "))
+n2=int(input("enter b value n2 "))
+
+if(n1>n2):
+    print("\n n1 is big ",n1)
+else:
+    print("\n n2 is big ",n2)
+
+if(n1<n2):
+    print("\n n1 is small ",n1)
+else:
+    print("\n n2 is small ",n2)
+
+if(n1<=n2):
+    print("\n n1 is greater n2 ",n1)
+else:
+    print("\n n2 is greater n1 ",n2)
+
+if(n1>=n2):
+    print("\n n1 is smaller n2 ",n1)
+else:
+    print("\n n2 is more are than equals to n1 ",n2)
+
+if(n1==n2):
+    print("\n both are equal ",n1==n2)
+else:
+    print("\n both are not equal ",n2!=n1)
+    
