7ee6fef0059ec415e942c089147026c37d73e66a
ravindra <aravindkumar.indus@gmail.com>
2024-07-15 17:19:18 +0530

    commit

---
+++ practice.py
@@ -0,0 +1,7 @@
+a = int(input("\n enter a value "))
+b = int(input("\n enter b value "))
+
+if (a>b):
+    print("\n a is big ")
+    else:
+    print("\n b is big ")
---
+++ true-while.py
@@ -0,0 +1,6 @@
+while True:
+user_input = input(" enter any number greater than 10 ")
+number = int(user_input)
+if number > 10 :
+    break
+print(" valid number entered ", number)
