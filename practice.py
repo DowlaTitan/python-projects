6ae6d1cef4f1017a9c900c2723cba67c6f12f3b2
ravindra <aravindkumar.indus@gmail.com>
2024-07-15 18:04:13 +0530

    commit

--- practice.py
+++ practice.py
@@ -1,7 +1,8 @@
-valid =false
+valid = False
+
 while not valid:
     user_input = input(" enter user name ")
-if len(user_input) >=5:
-    valid = true 
+    if len(user_input) >=5:
+        valid = True 
 
-    print("user name is valid ", user_input)
+print("user name is valid ", user_input)
--- while-false.py
+++ while-false.py
@@ -3,7 +3,7 @@ valid = False
 while not valid:
     user_input = input("Enter your username: ")
     if len(user_input) >= 5:
-        valid = True  # Set valid to True if the condition is met
+        valid = True 
 
 print("Username accepted:", user_input)
  
